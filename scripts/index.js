d2a0858c3fe54c5bab87120ac52e3d26acd4cc5a
Eof.Light <havdigy@gmail.com>
2022-06-30 10:50:31 +0700

    3d commit

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,23 @@ fullscreenMenu.querySelector('#fmenu__close').addEventListener('click', function
     event.preventDefault();
     fullscreenMenu.style.display='none';
 })
+
+const gearButton = document.querySelector(".shop__func");
+const specsListElement = document.querySelector(".shop__specs");
+gearButton.addEventListener("mouseover", e => {
+  specsListElement.style.opacity = "1";
+  specsListElement.style.top = "0";
+  
+})
+gearButton.addEventListener("mouseout", e => {
+  specsListElement.style.opacity = "0"
+  specsListElement.style.top = "-999999px"
+})
+specsListElement.addEventListener("mouseover", e => {
+  specsListElement.style.opacity = "1";
+  specsListElement.style.top = "0";
+})
+specsListElement.addEventListener("mouseout", e => {
+  specsListElement.style.opacity = "0"
+  specsListElement.style.top = "-999999px"
+})
