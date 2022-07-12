7ea09cd2d97a5a714f1eae62289a79deb87db692
EofLight <81947251+EofLight@users.noreply.github.com>
2022-07-12 22:41:14 +0700

    final

--- scripts/colors.js
+++ scripts/colors.js
@@ -15,21 +15,28 @@ $(".color__name").click(e=>{
 
     const winWidth=$(window).width();
     const needWidth= winWidth - ($this.width() * 3);
-
+    const sectionWidth = $this.closest(".section").width();
 
     const curContent=$this.siblings(".colors__content");
-    //console.log(curContent);
-
+    const textBlock=curContent.find(".colors__content-block");
+    const textWidthMobile = needWidth - parseInt(textBlock.css("padding-left")) + parseInt(textBlock.css("padding-right"));
+    const textWidthDesktop = (sectionWidth-(300+($this.width() * 3))) - (parseInt(textBlock.css("padding-left")) + parseInt(textBlock.css("padding-right")));
     closeEvery(allContent,curContent);
     
     if(!curContent.hasClass("active")){
         curContent.addClass("active");
-        if(winWidth<768){
+        textBlock.width(textWidthDesktop);
+        curContent.width(textWidthDesktop+ (parseInt(textBlock.css("padding-left")) + parseInt(textBlock.css("padding-right"))));
+        if(winWidth<=768){
+            textBlock.width(textWidthMobile);
             curContent.width(needWidth);
+            
         }
     } else{
         curContent.removeClass("active");
         curContent.width(0);
+        
+        
     }
 /*
     if(!$this.hasClass("active")){
